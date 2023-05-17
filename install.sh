4eb5b113648c8fb6c49ca0b99ca8a5652c0c2fc4
Eric Hugoson <eric.hugoson@gu.se>
2023-05-17 16:55:38 +0200

    added alacritty conf

--- install.sh
+++ install.sh
@@ -4,9 +4,12 @@ mkdir -p ~/.config/nvim
 
 # TODO: check for realpath, add alternative
 ln -s $(realpath ./zshrc) ~/.zshrc
+ln -s $(realpath ./alacritty.yml) ~/.config/alacritty/alacritty.yml
 ln -s $(realpath ./init.vim) ~/.config/nvim/init.vim
 ln -s $(realpath ./init.vim) ~/.vimrc
 ln -s $(realpath ./tmux.conf) ~/.tmux.conf
 ln -s $(realpath ./gitconfig) ~/.gitconfig
 
 # TODO: add case for X
+#cp ./udevmon.yaml /etc/interception/udevmon.d
+#cp ./interception_mappings.yaml /etc/interception/interception_mappings.yaml
