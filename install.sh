3d7c733c05637dab57bb64eb96ce625717298419
Eric Hugoson <eric@hugoson.org>
2023-08-01 21:17:40 +0200

    zshconf

--- install.sh
+++ install.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 mkdir -p ~/.config/nvim
+mkdir ~/zsh
 
 # TODO: check for realpath, add alternative
-ln -s $(realpath ./zshrc) ~/.zshrc
-ln -s $(realpath ./alacritty.yml) ~/.config/alacritty/alacritty.yml
-ln -s $(realpath ./init.vim) ~/.config/nvim/init.vim
-ln -s $(realpath ./init.vim) ~/.vimrc
-ln -s $(realpath ./tmux.conf) ~/.tmux.conf
-ln -s $(realpath ./gitconfig) ~/.gitconfig
+ln -sf $(realpath ./zshrc) ~/.zshrc
+ln -sf $(realpath ./alacritty.yml) ~/.config/alacritty/alacritty.yml
+ln -sf $(realpath ./init.vim) ~/.config/nvim/init.vim
+ln -sf $(realpath ./init.vim) ~/.vimrc
+ln -sf $(realpath ./tmux.conf) ~/.tmux.conf
+ln -sf $(realpath ./gitconfig) ~/.gitconfig
+ln -sf $(realpath ./zsh_config) ~/zsh/zsh_config
+ln -sf $(realpath ./zsh/zsh_config) ~/zsh/zsh_config
 
 # TODO: add case for X
 #cp ./udevmon.yaml /etc/interception/udevmon.d
